656b98f339da00dc4166113eafb5108f5381914b
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-09-29 20:54:59 +0100

    solve the detect a cycle challenge using C++

---
+++ Interview-Preparation-Kit/Linked-Lists/ Linked-Lists-Detect-a-Cycle.cpp
@@ -0,0 +1,25 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as:
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head) {
+  if (head == NULL) return false;
+
+  Node* slow = head;
+  Node* fast = head->next;
+
+  while ((slow != NULL) && (fast != NULL) && (fast->next != NULL)) {
+    if (slow == fast) return true;
+
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+
+  return false;
+}
