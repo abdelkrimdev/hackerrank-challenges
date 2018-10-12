359fd074c8cff4007f610c8a8b6640aef6f8aefe
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-10-12 21:49:07 +0100

    submitting day 13

---
+++ 30-Days-of-Code/Day-13-Abstract-Classes.cs
@@ -0,0 +1,17 @@
+//Write MyBook class
+class MyBook: Book
+{
+  private int price;
+
+  public MyBook(string title, string author, int price) : base(title, author)
+  {
+    this.price = price;
+  }
+
+  public override void display()
+  {
+    Console.WriteLine($"Title: {title}");
+    Console.WriteLine($"Author: {author}");
+    Console.WriteLine($"Price: {price}");
+  }
+}
